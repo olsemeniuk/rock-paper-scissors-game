f58ce5a2a776ef8b3eb2f236bb1e2f862df6f564
Oleksandr Semeniuk <127764709+olsemeniuk@users.noreply.github.com>
2026-08-16 14:25:48 +0300

    Add function to get random computer choice

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function getComputerChoice() {
+  const randomNum = Math.floor(Math.random() * 3) + 1;
+  const computerChoice =
+    randomNum === 1 ? "rock" : randomNum === 2 ? "paper" : "scissors";
+  return computerChoice;
+}
